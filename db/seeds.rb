64e29c883eb57661347295c17123366c37b9bf64
Victor Noual <victor.noual@orange.fr>
2020-05-12 13:07:40 +0200

    we have created following: card, bikes controller, bikes index view, bikes route

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: "Victor", password: "123456", email: "victor@pissendekuh.de")
+
+Category.create(name: "Mountain Bike")
+
+radl = Bike.create([{ user_id: 1, model: "101", price: 15, description: "The definition of a bicycle is a two-wheeled vehicle with one wheel in front of the other which is propelled using foot pedals.
+  A two-wheeled vehicle that you pedal is an example of a bicycle.
+  Bicycle is defined as to ride a two-wheeled vehicle that you propel forward by pedaling with your feet.
+  When you ride a two-wheeled vehicle
+   (also called a bicycle), this is an example of a time when you bicycle.", category_id: 1},
+   { user_id: 1, model: "35E0554", price: 150, description: "this bike
+  does this and that and it is very convienient and nice and it will impress your parents and all people around.",
+  category_id: 1}])
