beabb9d5b6068946652526102b0ceec02098af3e
Benedikt Richter <richter-benedikt@t-online.de>
2020-05-13 11:40:18 +0200

    Added user avatar and changed navbar style

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -32,7 +32,7 @@ class BikesController < ApplicationController
   private
 
   def bike_params
-    params.require(:bike).permit(:model, :brand, :description, :address, :price, :category)
+    params.require(:bike).permit(:model, :brand, :description, :address, :price, :category, :photo)
   end
 
   def set_bike
